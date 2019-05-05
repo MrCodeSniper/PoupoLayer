ee4a8c39abf649be4fd70dbbdb2108dfeff29875
a568261070 <woshichenhong12@gmail.com>
2019-05-05 18:33:37 +0800

    添加Popdialog构造方法

--- poplayer/src/main/java/com/github/codesniper/poplayer/custom/newPop/PopDialog.java
+++ poplayer/src/main/java/com/github/codesniper/poplayer/custom/newPop/PopDialog.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
@@ -23,6 +24,14 @@ public class PopDialog extends Dialog implements IPop, IWindow<Dialog> {
 
     public PopDialog(@NonNull Context context) {
         super(context);
+    }
+
+    public PopDialog(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    public PopDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
     }
 
     @Override
